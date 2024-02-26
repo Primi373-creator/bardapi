f179ec9fa116e0d624da681fe5ca73eb3ebfd549
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-02-26 05:04:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.all('/bard-query', async (req, res) => {
     const assistant = new BardAPI();
 
     // Set session information for authentication
-     await assistant.setSession('__Secure-1PSID', 'g.a000gwiHOnk5U6X3bztO5FvVkCAkf7edI5LKBixDZW-jl_dlrLA0QkwwaFs2sVqZPpSJUl7JowACgYKAScSAQASFQHGX2MiVVBl4NcuPzSyld5nZq0nUBoVAUF8yKp5bQiu6yVRvAISzvjeUbG-0076'); // or '__Secure-3PSID'
+     await assistant.setSession('__Secure-3PSID', 'g.a000gwiHOnk5U6X3bztO5FvVkCAkf7edI5LKBixDZW-jl_dlrLA0cNTfkH6aM2vuSy86GN1wkAACgYKAXsSAQASFQHGX2MiqamkaGLON273B9LWtVNVUBoVAUF8yKrdfDx-80igen8Vp4CeWmy80076'); // or '__Secure-3PSID'
 
 
     // Send a query to Bard
